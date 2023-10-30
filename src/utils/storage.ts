ecd0dd8d3cc3039dc4e9d203b2fe84a4f263bdcf
Junior Albuquerque <junioralbuquerque81@gmail.com>
2023-10-29 21:43:51 -0300

    refactor: encryptstorage name

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,6 +1,6 @@
 import { EncryptStorage } from 'encrypt-storage'
 
-export const encryptStorage1 = new EncryptStorage(
+export const encryptStorage = new EncryptStorage(
   import.meta.env.VITE_SECRET_STORAGE,
   {
     prefix: '@dot-wars'
