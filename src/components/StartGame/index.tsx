6462f1e2899a1fd0fe8c0ab46a9bb63b1ed31856
Junior Albuquerque <junioralbuquerque81@gmail.com>
2023-10-30 00:03:03 -0300

    chore: add restar game online

--- src/components/StartGame/index.tsx
+++ src/components/StartGame/index.tsx
@@ -1,44 +1,61 @@
 import useSound from 'use-sound'
-import sunflower from '@/assets/sunflower.mp3'
+// import sunflower from '@/assets/sunflower.mp3'
 import unlock from '@/assets/material_product_sounds/wav/primary/ui_unlock.wav'
-import { useEffect, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
 import Logo from '../Logo'
 import { useGameStore } from '@/store/game/game.store'
 import { dropIn } from './variants'
 import { flexWrapper, playButtonStyle, startGameOverlay } from './styles.css'
 import { createPortal } from 'react-dom'
+import { useMutation } from 'convex/react'
+import { api } from '@convex/_generated/api'
+import { encryptStorage } from '@/utils/storage'
 
 const startGameRoot = document.getElementById('start-game-root') as HTMLElement
 
-export const StartGame = () => {
+type StartGameProps = {
+  online?: boolean
+}
+
+export const StartGame: FC<StartGameProps> = ({ online }) => {
   const [showStartGame, setShowStartGame] = useState(true)
   const winner = useGameStore((state) => state.winner)
   const resetGame = useGameStore((state) => state.resetGame)
 
-  const [play, { sound }] = useSound(sunflower, {
-    volume: 0.088
-  })
+  const restartOnlineGame = useMutation(api.rooms.restartRoom)
+
+  // const [play, { sound }] = useSound(sunflower, {
+  //   volume: 0.088
+  // })
   const [playClick] = useSound(unlock, {
     volume: 2
   })
 
-  const handlePressPlayButton = () => {
-    if (!winner) {
-      play()
+  const handlePressPlayButton = async () => {
+    if (online && winner) {
+      const room_id = encryptStorage.getItem('room')
+
+      await restartOnlineGame({
+        room_id
+      })
     }
 
-    resetGame()
+    if (winner) {
+      resetGame()
+    }
+
+    // resetGame()
     setShowStartGame(false)
   }
 
-  useEffect(() => {
-    if (sound) {
-      sound?.on('end', () => {
-        play()
-      })
-    }
-  }, [sound])
+  // useEffect(() => {
+  //   if (sound) {
+  //     sound?.on('end', () => {
+  //       play()
+  //     })
+  //   }
+  // }, [sound])
 
   useEffect(() => {
     if (winner) {
@@ -74,7 +91,7 @@ export const StartGame = () => {
 
             <motion.button
               className={playButtonStyle}
-              onMouseUp={() => handlePressPlayButton()}
+              onMouseUp={handlePressPlayButton}
               onMouseDown={() => {
                 playClick()
               }}
