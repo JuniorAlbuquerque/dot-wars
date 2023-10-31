ecfc91928db1aa5f5237817b9139f341932d2cb5
Junior Albuquerque <junioralbuquerque81@gmail.com>
2023-10-30 23:41:33 -0300

    chore: add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import '@/styles/layers.css.ts'
 import { ConvexProvider, ConvexReactClient } from 'convex/react'
+import { Analytics } from '@vercel/analytics/react'
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string)
 
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
   <ConvexProvider client={convex}>
     <App />
+    <Analytics />
   </ConvexProvider>
   // </React.StrictMode>,
 )
